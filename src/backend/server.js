a0e33894e8e4b8089162f7ae9eb4ac66519f74d1
haibinc <haibinc143@gmail.com>
2023-12-25 19:11:46 -0800

    testing

--- src/backend/server.js
+++ src/backend/server.js
@@ -286,15 +286,19 @@ app.delete('/deletePicture/:picTitle', async(req,res) => {
 
 app.post('/addLike/:picTitle', async (req, res) => {
     try{
+        console.log('NOTHING YET');
         const sqlSelect = "SELECT * FROM likes WHERE imageName = ?";
         const [rows, fields] = await dbLogin.execute(sqlSelect, [req.params.picTitle]);
+        console.log('oh dayumm');
         if(!rows){
+            console.log('SENDING INFO')
             const sqlInsert = "INSERT into likes (userId, imageName) VALUES (?, ?)";
             const [values] = [req.body, req.params.picTitle];
             await dbLogin.execute(sqlInsert, values);
             return res.status(200).send("Liked successfully");
         }
         else{
+            console.log('ERROR SPOT')
             return res.status(400).send("You already liked this");
         }
     }
