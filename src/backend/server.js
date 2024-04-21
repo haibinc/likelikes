d754046f6e302b6d0d102bd64b1d517c86525a4f
haibinc <haibinc143@gmail.com>
2024-04-20 22:31:59 -0700

    qw

--- src/backend/server.js
+++ src/backend/server.js
@@ -44,6 +44,7 @@ app.get("/", async (req, res) => {
 app.get('/.well-known/pki-validation/D7EA86285258ABA8816330CA044C7545.txt', async (req, res) => {
     try{
         res.send("DOES THIS WORK");
+        res.send("change")
         console.log('WHY');
         res.sendFile('/home/ec2-user/likelikes/src/backend/D7EA86285258ABA8816330CA044C7545.txt');
     } catch(error){
